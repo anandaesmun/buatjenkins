5b5d447abdc801ef1aa64db2ec0cd64970670bb3
agripramadio29 <119289271+agripramadio29@users.noreply.github.com>
2023-05-23 16:36:27 +0700

    test login baru

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,16 +4,36 @@ from django.urls import reverse
 
 class test_views(TestCase):
 
-    def test_home_GET(self):
+    def test_home_correct_credentials_GET(self):
         client = Client()
         response = client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
     
-    def test_logout_GET(self):
+    def test_home_different_username_GET(self):
         client = Client()
-        response = client.get(reverse('log_out'))
-        self.assertEqual(response.status_code, 302)
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    def test_home_different_password_GET(self):
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_home_different_username_password_GET(self):
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+    
+    # def test_logout_GET(self):
+    #     client = Client()
+    #     response = client.get(reverse('log_out'))
+    #     self.assertEqual(response.status_code, 302)
+    
+    
 
     # def test_count_GET(self):
     #     client = Client()
