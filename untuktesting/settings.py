37902347927960770a17d5a666fda278eec90fc6
agripramadio29 <119289271+agripramadio29@users.noreply.github.com>
2023-06-12 18:48:11 +0700

    ready for prod

--- untuktesting/settings.py
+++ untuktesting/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n#9xu8!jetmt-se%%4az!4$zu(3q%u^^-$jaa+@6@66daa*!9$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
